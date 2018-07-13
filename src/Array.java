4e86ebd243840b09199eae270669e8582e64e2cb
TrollSpace <miger2000@mail.ru>
2018-07-13 22:50:16 +0300

    Create interface

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Misha on 13.07.2018.
+ */
+public interface Array {
+
+     void addElement(int value);
+
+     boolean deleteElement(int value);
+
+     int findElement(int value);
+}
