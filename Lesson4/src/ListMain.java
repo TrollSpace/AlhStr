ffcf43d167d36d41d2e58b7ddd213a9d44efdbc8
TrollSpace <miger2000@mail.ru>
2018-07-22 23:08:57 +0300

    Add display, insert.

--- Lesson4/src/ListMain.java
+++ Lesson4/src/ListMain.java
@@ -1,7 +1,14 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Misha on 22.07.2018.
  */
 public class ListMain {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedListImpl();
+        list.insert(1);
+        list.insert(5);
+        list.insert(143);
+        list.insert(12);
+
+        list.display();
+    }
 }
