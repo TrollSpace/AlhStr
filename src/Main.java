0ef92d96aca644e498e140829a769094ec66ee30
Mikhail <miger2000@mail.ru>
2018-07-23 03:57:27 +0300

    Revert "Add Stack"
    This reverts commit 82222858dd14757551f453097975b1d044f6ca06.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,36 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
-/*
-    1. Реализовать рассмотренные структуры данных в консольных программах.
-    2. Создать программу, которая переворачивает вводимые строки (Читает справа налево).
-    3. Создать класс для реализации дека.
+/**
+ * 1. Создать массив большого размера (миллион элементов).
+ * 2. Написать методы удаления, добавления, поиска элемента массива.
+ * 3. Заполнить массив случайными числами.
+ * 4. Написать методы, реализующие рассмотренные виды сортировок и проверить скорость выполнения каждой.
  */
 public class Main {
     public static void main(String[] args) {
-        int size = 3;
-        Stack st = new StackImpl(size);
+        int size = 1000000; // на этом размере компьютер начинает заикаться =)
+        ArrayImpl numbers1 = new ArrayImpl(size);
+        ArrayImpl numbers2 = new ArrayImpl(size);
+        ArrayImpl numbers3 = new ArrayImpl(size);
         for (int i = 0; i < size; i++) {
-            int x = (int) (Math.random() * 100);
-            System.out.println(x);
-            st.push(x);
+            int x = new Random().nextInt(size);
+            numbers1.addElement(x);
+            numbers2.addElement(x);
+            numbers3.addElement(x);
         }
-
-        while (st.isEmpty()) {
-            System.out.println(st.pop());
-        }
-
+        long start = System.currentTimeMillis();
+        numbers2.bubbleSort();
+        long finish = System.currentTimeMillis();
+        System.out.println(finish-start);
+        start = System.currentTimeMillis();
+        numbers1.selectSort();
+        finish = System.currentTimeMillis();
+        System.out.println(finish-start);
+        start = System.currentTimeMillis();
+        numbers3.insertSort();
+        finish = System.currentTimeMillis();
+        System.out.println(finish-start);
 
     }
 }
