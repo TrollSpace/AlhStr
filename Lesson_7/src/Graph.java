b742d771626483f269b4169bf4ea4f2871e0c58f
TrollSpace <miger2000@mail.ru>
2018-08-02 00:00:42 +0300

    Add Vertex

--- Lesson_7/src/Graph.java
+++ Lesson_7/src/Graph.java
@@ -1,7 +1,7 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Misha on 01.08.2018.
  */
 public interface Graph {
+
+    boolean addVertex(Vertex vertex);
 }
--- Lesson_7/src/Vertex.java
+++ Lesson_7/src/Vertex.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Misha on 01.08.2018.
  */
 public class Vertex {
+    private String label;
+
+    public Vertex(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
 }
